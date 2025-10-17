b6faf1dff4d4be4aa2cf86d0a46a013dcfc166eb
yasingunay <av.yasingunay@gmail.com>
2025-10-17 23:53:19 +0200

    Update base path to lawyerwebsite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/lawyer__website/',
+  base: '/lawyerwebsite/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
